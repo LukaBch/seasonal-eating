d0e08efc49d006c11b31c454b8ad56dbbddbff20
Lucas <lucas.banchereau@gmail.com>
2022-08-04 17:17:35 +0200

    EatableFruits handle case only one fruit eatable

--- src/EatableFruits.tsx
+++ src/EatableFruits.tsx
@@ -11,5 +11,12 @@ export default function EatableFruits(props: IEatableFruitsProps) {
   if (eatableFruitNameList.length === 0) {
     return <div>Aucun fruit n'est de saison ce mois-ci !</div>;
   }
+  if (eatableFruitNameList.length === 1) {
+    return (
+      <div>
+        L'unique fruit de saison ce mois-ci est {eatableFruitNameList[0]}
+      </div>
+    );
+  }
   return <div>Les fruits de saison sont {eatableFruitNameList.join(", ")}</div>;
 }
